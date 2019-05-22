901f9aa05e9df8573c0f0c9b373af61940d4fdfc
Joy Lin <vvlnote@hotmail.com>
2019-05-22 22:15:18 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190522221141) do
+ActiveRecord::Schema.define(version: 20190522221413) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20190522221141) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.boolean  "post_status"
+    t.string   "post_status"
   end
 
 end
